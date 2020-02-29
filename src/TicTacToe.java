f38eb445fe6890a9d13d546ac837ad304aff16e6
Imran01000 <imshaik01000@gmail.com>
2020-02-29 16:31:06 +0530

    ADDED:Added feature to know user which letter assigned to him.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,14 +1,33 @@
 import java.util.*;
 public class TicTacToe {
+	
+	//Declaring 2D array for getting board.
 	static int[][] arrayBoard=new int[3][3];
+	static Random r=new Random();
+	
+	//To know the if letter X or O is assigned to me.
+	private static String getLetter() 
+	{
+		int randomCheck=r.nextInt(2);
+		if(randomCheck==0)
+		return "you will ahead with 'O'";
+		else
+		return "You will ahead with 'X'";
+		
+	}
 	public static void main(String[] args)
 	{
-		for (int i = 0; i < arrayBoard.length; i++) {
-			for (int j = 0; j < arrayBoard.length; j++) {
+		for (int i = 0; i < arrayBoard.length; i++) 
+		{
+			for (int j = 0; j < arrayBoard.length; j++) 
+			{
 				System.out.print(arrayBoard[i][j]+" ");	
 			}
 			System.out.println();
 		}
-	
+		String letter=getLetter();
+		System.out.println(letter);
+		
 	}
+
 }
