7098f686b426edc0e0569e23253bd231ddad80e6
Imran01000 <imshaik01000@gmail.com>
2020-02-29 17:07:27 +0530

    ADDED:Added feature wheather the user won toss or loss .

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,21 +1,52 @@
 import java.util.*;
 public class TicTacToe {
+
+	// variables
+	static int random=0;
+	static int randomCheck=0;
+	static String letter="";
+	static String checkToss="";
+	static int head=0;
+	static int tail=1;
 	
 	//Declaring 2D array for getting board.
 	static int[][] arrayBoard=new int[3][3];
 	static Random r=new Random();
 	
+	//To generate random number.
+	private static int getRandomNumber() {
+		random=r.nextInt(2);
+		return random;
+		
+	}
+	
 	//To know the if letter X or O is assigned to me.
 	private static String getLetter() 
 	{
-		int randomCheck=r.nextInt(2);
+		randomCheck=getRandomNumber();
 		if(randomCheck==0)
 		return "you will ahead with 'O'";
 		else
 		return "You will ahead with 'X'";
 		
 	}
-	public static void main(String[] args)
+	
+	//To get the toss result.
+	private static String getToss() 
+	{
+		randomCheck=getRandomNumber();
+		if(randomCheck==head)
+		{
+			return "You won the toss congratulations go ahead now....";
+		}
+		else
+		{
+			return "You lost the toss wait for your turn.... ";
+		}
+	}		
+	
+	//To display array as board.
+	private static void arrayDisplay()
 	{
 		for (int i = 0; i < arrayBoard.length; i++) 
 		{
@@ -25,9 +56,18 @@ public class TicTacToe {
 			}
 			System.out.println();
 		}
-		String letter=getLetter();
+	}
+	
+	public static void main(String[] args)
+	{
+		arrayDisplay();
+		letter=getLetter();
 		System.out.println(letter);
+		checkToss=getToss();
+		System.out.println(checkToss);
 		
 	}
+
+	
 
 }
